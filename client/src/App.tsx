cd9935b80b0c632ecf5175762e6864ad904a49df
f0ring <farinmaishaa110@gmail.com>
2026-03-31 01:50:16 +0600

    Added an admin page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,6 +6,7 @@ import Home from './views/Home';
 import Login from './views/Login';
 import Categories from "./views/Categories";
 import MyLibrary   from './views/MyLibrary';
+import Admin       from './views/Admin';
 import Signup from './views/Signup';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
@@ -26,6 +27,7 @@ function App() {
           <Route path="/login" element={<Login />} />
           <Route path="/books"      element={<Books />} />
           <Route path="/my-library" element={<MyLibrary />}  />
+          <Route path="/admin"      element={<Admin />}      />
           <Route path="/categories" element={<Categories />} />
           <Route path="/about"      element={<About />}      />
 
